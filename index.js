4a299cdd6b012f8fa9d22e144e4be1ef6fc5bb91
kaique <kaiquesilva49@hotmail.com>
2022-07-10 10:25:07 -0300

    configurando lint

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-import chalk from 'chalk';
-import fs from 'fs';
+import chalk from 'chalk'
+import fs from 'fs'
 
 function trataErro(erro) {
-    throw new Error(erro.code);
+    throw new Error(erro.code)
 }
 
-function pegaArquivo(caminho) {
-    const encoding = 'utf-8';
-    fs.promises
-    .readFile(caminho,encoding)
-    .then((texto)=>console.log(chalk.green(texto)))
-    .catch((erro)=>trataErro(erro))
+async function pegaArquivo(caminho) {
+    try {
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminho, encoding)
+        console.log(texto)
+    } catch (erro) {
+        trataErro(erro)
+    }
 }
 
-pegaArquivo("./arquivos/teste.md");
+// function pegaArquivo(caminho) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//     .readFile(caminho,encoding)
+//     .then((texto)=>console.log(chalk.green(texto)))
+//     .catch((erro)=>trataErro(erro))
+// }
+
+pegaArquivo('./arquivos/teste.md')
